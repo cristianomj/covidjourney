25e3c8bfd4f48dabff8be2fb3a10b2792a0d4390
Cristiano Miranda <cristianocmj@gmail.com>
2020-03-30 21:16:14 -0400

    Add frontend

--- covidjourney/settings.py
+++ covidjourney/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Disable browseable API in production
